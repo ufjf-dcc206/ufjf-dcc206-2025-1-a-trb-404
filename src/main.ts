8d652dacf9d3f4d9a6813d7afb1a41288e92c763
JG-Souza <joaogsouzafma@gmail.com>
2025-07-27 15:38:19 -0300

    Limpa o projeto removendo arquivos desnecessários e configura estrutura básica de HTML e CSS

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,9 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import "./styles/main.css";
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+const app = document.querySelector<HTMLDivElement>("#app");
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+if (app) {
+  app.innerHTML = `
+    <h1>ICElatro</h1>
+  `;
+}
