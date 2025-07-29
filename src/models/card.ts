39b82a1e144c6425a3236d5688300c4e68c35490
Gabriel <gabriellegalizado9@gmail.com>
2025-07-29 11:21:43 -0300

    comentei uma option no tsconfig que tava conflitando

---
+++ src/models/card.ts
@@ -0,0 +1,16 @@
+/** 
+ * @enum cardSuits
+ * @description enumeração dos naipes das cartas
+ */
+/** criei essa enumeração dos naipes das carta pra usar no cardcomponents */
+export enum cardSuits {
+    Copas = 'copas',
+    Ouro = 'ouro',
+    Paus = 'paus',
+    Espada = 'espada'
+}
+
+/**
+ * @enum cardValues
+ * @description Enumeração para representar os valores das cartas, incluindo figuras.
+ */
